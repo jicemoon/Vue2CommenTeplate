3694b308aa4c8afa85ba589b126aa3748e127338
jicemoon <jicemoon@outlook.com>
2016-12-09 14:06:07 +0800

    reset Header Menu

--- Scripts/Default/Main.js
+++ Scripts/Default/Main.js
@@ -2,23 +2,24 @@
  * Created by jicemoon on 2016/12/8.
  */
 new Vue({
-  el: "#root",
-  data: function (){
-    return {
-      labelList:[{label:"jicemoon", percent: 50}],
-      currentView: ""
-    };
-  },
-  methods: {
-    labelSuccess: function (label, type) {
-      console.log(type, label);
-      console.log(this.labelList);
+    el: "#root",
+    data: function () {
+        return {
+            root: this,
+            labelList: [{label: "jicemoon", percent: 50}],
+            currentView: "hello"   //默认
+        };
     },
-    labelFail: function (message, type) {
-      alert(message);
-    },
-    TabMenu: function (compName){
-      this.currentView = compName;
+    methods: {
+        labelSuccess: function (label, type) {
+            console.log(type, label);
+            console.log(this.labelList);
+        },
+        labelFail: function (message, type) {
+            alert(message);
+        },
+        TabMenu: function (compName) {
+            this.currentView != compName && (this.currentView = compName);
+        }
     }
-  }
 });
--- Scripts/Default/components/HeaderMenu.js
+++ Scripts/Default/components/HeaderMenu.js
@@ -1,22 +1,29 @@
 /**
  * Created by jicemoon on 2016/12/8.
  */
-(function (){
-  var Name = "header-menu";
-  var htmlUrl = "/Scripts/Default/components/HeaderMenu.html";
-  var InitData = {
-    name: Name,
-    data: function () {
-      return {
-        currentView:""
-      }
-    },
-    methods:{
-      TabMenu: function (current){
-        this.currentView = current;
-        this.$emit("tab", current);
-      }
-    }
-  };
-  Utils.RegistrationComponents(Name, InitData, htmlUrl);
+(function () {
+    var Name = "header-menu";
+    var htmlUrl = "/Scripts/Default/components/HeaderMenu.html";
+    var InitData = {
+        name: Name,
+        props: {
+            "tab-menu":{
+                type: Function,   //切换Menu的监听函数
+                required: true
+            },
+            "root":Object
+        }//,
+        // data: function () {
+        //     return {
+        //         //currentView: ""
+        //     }
+        // },
+        // // methods: {
+        // //     TabMenu: function (current) {
+        // //         this.root.currentView = current;
+        // //         //this.$emit("tab", current);
+        // //     }
+        // // }
+    };
+    Utils.RegistrationComponents(Name, InitData, htmlUrl);
 })();
